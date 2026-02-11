34dbc460672940019b234a1b2e378a248fd05e7c
ZuyVladislav <vlados9597@mail.ru>
2026-02-11 08:05:12 +0300

    update

--- crypto_util.py
+++ crypto_util.py
@@ -8,7 +8,7 @@ from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-
+from cryptography.hazmat.primitives import serialization
 
 def hkdf_sha256(ikm: bytes, salt: bytes, info: bytes, length: int = 32) -> bytes:
     return HKDF(algorithm=hashes.SHA256(), length=length, salt=salt, info=info).derive(ikm)
@@ -30,8 +30,11 @@ def xpub_bytes(pub: x25519.X25519PublicKey) -> bytes:
     return pub.public_bytes_raw()
 
 
-def xpub_from_bytes(b: bytes) -> x25519.X25519PublicKey:
-    return x25519.X25519PublicKey.from_public_bytes(b)
+def xpub_bytes(pub: x25519.X25519PublicKey) -> bytes:
+    return pub.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw,
+    )
 
 
 @dataclass
